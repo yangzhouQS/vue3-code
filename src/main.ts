e5fda39f23baa8a23880fddabc7b415403e5f8aa
yangzhou <yearrow@qq.com>
2023-11-10 15:00:57 +0800

    fix: 递归渲染优化

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,31 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import {createFormily} from "@/components/vue-formily";
 
+window.Vue = {
+  ...Vue,
+  resolveComponent(...args) {
+    // 此处先执行vue内部的解析组件的方法，如果可以拿到组件对象则直接返回，反之则去注册区块
+    const component = Vue.resolveComponent(args[0])
+    /*if (component && typeof component === 'string') {
+      return getComponent(capitalize(camelize(args[0])))
+    } else {
+      return component
+    }*/
+    console.log(component);
+    debugger;
+    return component
+  },
+  // renderSlot方法第三个参数是作用域插槽传递的数据，格式{ data: vue.unref(state).componentData }
+  renderSlot(...args) {
+    // 获取当前vue的实例
+    const instance = Vue.getCurrentInstance()
+    console.log(args);
+  }
+}
+
+
 const formily = createFormily();
-const app = createApp(App)
+const app = Vue.createApp(App)
 
 /*Vue.resolveComponent = function () {
   debugger;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,13 +6,13 @@ declare module '*.vue' {
   export default component
 }
 
-declare module "*.tsx" {
-  /*interface HTMLAttributes<T> {
+/*declare module "*.tsx" {
+  /!*interface HTMLAttributes<T> {
     class: any;
 
     [key: string]: any
-  }*/
+  }*!/
   import type {DefineComponent} from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
-}
+}*/
