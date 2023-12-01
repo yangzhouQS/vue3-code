b5b559ee5b92a2e7088bd55107bae99c8f652f76
yangzhou <2638497222@qq.com>
2023-12-02 00:40:49 +0800

    UnoCSS

---
+++ src/pages/unocss-page/unocss-page.tsx
@@ -0,0 +1,38 @@
+import {defineComponent, onMounted, ref} from "vue"
+
+export const UnocssPage = defineComponent({
+  name: 'UnocssPage',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  setup() {
+    // data
+    const menuConfig = ref([])
+
+    // methods
+    const methods = {
+      loadData: () => {
+
+      }
+    }
+
+    onMounted(() => {
+
+    })
+
+    return () => {
+      return (
+        <div class={'full-container'}>
+          UnocssPage
+          <div class="text-right">
+            csddcs
+          </div>
+          <span class="p-5">span</span>
+        </div>
+      )
+    }
+  }
+})
--- src/router/index.ts
+++ src/router/index.ts
@@ -21,6 +21,7 @@ import {TablePage} from "@/pages/table/table";
 import Dropdown from "@/pages/dropdown/dropdown.vue";
 import {FlowPage} from "@/pages/flow/flow";
 import {MainPage} from "@/pages/main-page";
+import {UnocssPage} from "@/pages/unocss-page/unocss-page";
 
 export const routes = [
   {path: '/', component: MainPage},
@@ -44,6 +45,7 @@ export const routes = [
   {path: '/table-page', component: TablePage, title: "表格"},
   {path: '/dropdown', component: Dropdown, title: "下拉菜单调试"},
   {path: '/flow-page', component: FlowPage, title: "BPMN"},
+  {path: '/UnocssPage', component: UnocssPage, title: "UnocssPage"},
 ]
 export const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
