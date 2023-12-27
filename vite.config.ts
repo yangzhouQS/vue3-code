7f6addd84cbfb13e526590c06b78f4b28899ba9f
yangzhou <2638497222@qq.com>
2023-12-28 00:45:48 +0800

    fix: x

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,48 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path"
 import vueJsx from "@vitejs/plugin-vue-jsx"
-import UnoCSS from 'unocss/vite'
+import UnoCSS from 'unocss/vite';
+import externalGlobals from "rollup-plugin-external-globals";
+import {autoComplete, Plugin as importToCDN} from "vite-plugin-cdn-import";
+
 const resolve = (dir) => {
   return path.resolve(__dirname, dir)
 }
+
+// 全局对象
+let globals = externalGlobals({
+  vue: "Vue",
+  // vuex: "Vuex",
+  // vueRouter: "VueRouter",
+  // "element-plus": "element"
+})
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // globals,
     // UnoCSS(),
-    vueJsx({})
+    vueJsx({}),
+    importToCDN({
+      modules: [
+        /*autoComplete('vue'),
+        {
+          name:"vue",
+          var:"Vue",
+          path:'./vue.global.js'
+        }*/
+      ]
+    })
   ],
   test: {
-    globals: true,
-    // 配置测试环境，因为测试是在node下进行的，所以需要模拟浏览器环境，按照jsdom和happydom都是可以的
-    environment: 'jsdom',
-    testTransformMode: {
-      web: [/\.[jt]sx$/]
-    }
+    /* globals: true,
+     // 配置测试环境，因为测试是在node下进行的，所以需要模拟浏览器环境，按照jsdom和happydom都是可以的
+     environment: 'jsdom',
+     testTransformMode: {
+       web: [/\.[jt]sx$/]
+     }*/
   },
   define: {
     'process.env': Object.assign({}, process.env)
@@ -32,6 +56,19 @@ export default defineConfig({
       'pages': resolve('src/pages'),
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['vue'],
+      output:{
+        globals: {
+          vue: 'Vue',
+        },
+      },
+      plugins: [
+        // globals
+      ],
+    },
   },
   server: {
     // 服务器主机名，如果允许外部访问，可设置为 "0.0.0.0"
