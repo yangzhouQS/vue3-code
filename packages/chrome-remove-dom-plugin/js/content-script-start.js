7123d8148a36ed169b880353ae86dcaf73603293
yangzhou <yearrow@qq.com>
2025-12-14 10:55:14 +0800

    feat(chrome-remove-dom-plugin): 支持掘金网站DOM元素移除
    - 在hostList中新增juejin.cn
    - 为juejin.cn实现代码块及多种页面元素的移除逻辑
    - 针对juejin.cn添加伪元素样式覆盖，移除codeLine前置内容
    - 清理manifest.json中无用权限，保持权限最小化
    - 补充README.MD中默认配置示例及权限说明

--- packages/chrome-remove-dom-plugin/js/content-script-start.js
+++ packages/chrome-remove-dom-plugin/js/content-script-start.js
@@ -1,5 +1,5 @@
 $(function () {
-	const hostList = ['help.primeton.com']
+	const hostList = ['help.primeton.com', "juejin.cn"]
 	const $openButton = $("<button>remove</button>");
 	$openButton.css({
 		position: "fixed",
@@ -18,38 +18,64 @@ $(function () {
 		'help.primeton.com': () => {
 
 			// 锚点移除
-			;[...document.querySelectorAll('.header-anchor')].forEach(item=>{item.remove()});
+			;[...document.querySelectorAll('.header-anchor')].forEach(item => {
+				item.remove()
+			});
 
 			const navbar = document.querySelector(".navbar")
-			if (navbar){
+			if (navbar) {
 				navbar.remove();
 				console.log('navbar:移除成功');
 			}
 
 			const sidebar = document.querySelector(".sidebar")
-			if (sidebar){
+			if (sidebar) {
 				sidebar.remove();
 				console.log('sidebar:移除成功');
 			}
 
 			const pageAnchor = document.querySelector(".page-anchor")
-			if (pageAnchor){
+			if (pageAnchor) {
 				pageAnchor.remove();
 				console.log('pageAnchor:移除成功');
 			}
 
 			const pageNav = document.querySelector(".page-nav")
-			if (pageNav){
+			if (pageNav) {
 				pageNav.remove();
 				console.log('pageNav:移除成功');
 			}
 			const globalUi = document.querySelector(".global-ui")
-			if (globalUi){
+			if (globalUi) {
 				globalUi.remove();
 				console.log('globalUi:移除成功');
 			}
 
 			$openButton.remove();
+		},
+		"juejin.cn": () => {
+			;[...document.querySelectorAll('.code-block-extension-header')].forEach(item => {
+				item.remove()
+			});
+
+			function removeBefore() {
+				// 创建style标签并插入<head>
+				const style = document.createElement('style');
+				document.head.appendChild(style);
+				const sheet = style.sheet;
+
+				// 插入覆盖规则（优先级需高于原有样式）
+				sheet.insertRule(`
+      .code-block-extension-codeLine::before {
+        content: none !important;
+      }
+    `, 0);
+			}
+
+			removeBefore();
+			[".article-suspended-panel", '.main-header', '.sidebar', '.comment-box-common', '.recommended-area', '.suspension-panel'].forEach(dom => {
+				$(dom).remove()
+			})
 		},
 	}
 
